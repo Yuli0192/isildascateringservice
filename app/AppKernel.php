d8c290b86c8c4b69c3a0d7b0fdb920899176011c
Yuliana.arias <Yuliana.arias@Programador-PC4.desarrollos.co.cr>
2012-11-19 15:39:23 -0600

    Configuracion de Symfony2 y bundle ActivDevNocline

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,12 +13,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new ICS\DashboardBundle\ICSDashboardBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -26,6 +25,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new ActivDev\NoclineBundle\ActivDevNoclineBundle();
         }
 
         return $bundles;
---
+++ src/ICS/DashboardBundle/ICSDashboardBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ICS\DashboardBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Clase que se encarga de crear la instancia del Bundle DemandPlan.
+ */
+class ICSDashboardBundle extends Bundle
+{
+}
